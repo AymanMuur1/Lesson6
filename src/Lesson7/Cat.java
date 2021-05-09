492b7d4f054c4e9e9aeb5e0df960f9be992d30f3
AymanMuur1 <82612758+AymanMuur1@users.noreply.github.com>
2021-05-09 21:49:07 +0300

    Added configuration files

---
+++ src/Lesson7/Cat.java
@@ -0,0 +1,16 @@
+package Lesson7;
+
+public class Cat {
+    private String name;
+    private int appetit;
+
+    public Cat(String name, int appetit) {
+        this.name = name;
+        this.appetit = appetit;
+    }
+    public void eat(Plate p){
+        System.out.println("Кот начал есть");
+        p.decreaseFood(appetit);
+
+    }
+}
